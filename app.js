32469e6075500e2fd253809ae2964729887c22a0
vishakha1801 <43349452+vishakha1801@users.noreply.github.com>
2020-06-30 17:37:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB ='mongodb+srv://vishakha:helloworld@cluster0-yajjn.mongodb.net/local_library?retryWrites=true&w=majority'
+var dev_db_url ='mongodb+srv://vishakha:helloworld@cluster0-yajjn.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
